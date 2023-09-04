8952cd6d74d389282fb51c296c9c439a8b584946
Gabrielbmm <gabrielmazzollamorais@gmail.com>
2023-09-03 21:57:34 -0300

    version 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Home from './pages/home/home';
+import Home2 from './pages/home2/home2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Home/>} />
+          <Route path={"/home2"} element={<Home2/>} />
+        </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/home2/home2.jsx
@@ -0,0 +1,6 @@
+const Home2 = () => { 
+    return (
+      <h1>Página Home2</h1>  
+    );
+}
+export default Home2
